03e8924ec92419e4fdcc356da1be667018907855
cmacspeedo <cmacspeedo101@gmail.com>
2023-01-25 17:08:40 -0700

    fixed read about Brett link with preventDefault change in JS

--- js/script.js
+++ js/script.js
@@ -34,27 +34,56 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
     // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+    if (href === "#") e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
 
     // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== "#" && href.startsWith("#")) e.preventDefault();
+    {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+    if (link.classList.contains("main-nav-link")) e.preventDefault();
+    headerEl.classList.toggle("nav-open");
   });
 });
+
+// ///////////////////////////////////////////////////////////
+// // Smooth scrolling animation
+
+// const allLinks = document.querySelectorAll("a:link");
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
+
+//     // Close mobile naviagtion
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
